34692882cd3c572fef9768a653b22085952eb2ef
David Straub <straub@protonmail.com>
2025-06-12 07:44:55 +0200

    Use memory backend for test

--- test/test_importer.py
+++ test/test_importer.py
@@ -6,5 +6,5 @@ def test_importer_maximal70():
     # Test the import_gedcom function with a maximal GEDCOM 7.0 file
     gedcom_file = "test/data/maximal70.ged"
     db = make_database("sqlite")
-    db.load("test_db")
+    db.load(":memory:")
     import_gedcom(gedcom_file, db)
