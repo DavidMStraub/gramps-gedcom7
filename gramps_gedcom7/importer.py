c411255daf96255d314b02cb6dfe6f6669486248
David Straub <straub@protonmail.com>
2025-06-19 12:53:01 +0200

    Add Gramps import function

--- gramps_gedcom7/importer.py
+++ gramps_gedcom7/importer.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from gramps.gen.db import DbWriteBase
+from gramps.gen.user import UserBase
 import gedcom7
 from pathlib import Path
 from typing import TextIO, BinaryIO
@@ -15,8 +16,8 @@ def import_gedcom(file_input: str | Path | TextIO | BinaryIO, db: DbWriteBase) -
 
     Args:
 
-        - file_input: The GEDCOM file to import. This can be a string, Path object, or file-like object.
-        - db: The Gramps database to import the GEDCOM file into.
+        file_input: The GEDCOM file to import. This can be a string, Path object, or file-like object.
+        db: The Gramps database to import the GEDCOM file into.
     """
     # Check if file_input is a string or Path object
     if isinstance(file_input, (str, Path)):
@@ -33,3 +34,16 @@ def import_gedcom(file_input: str | Path | TextIO | BinaryIO, db: DbWriteBase) -
 
     gedcom_structures = gedcom7.loads(gedcom_data)
     process.process_gedcom_structures(gedcom_structures, db)
+
+
+def import_gedcom_gramps(database: DbWriteBase, filename: str, user: UserBase):
+    """Import a GEDCOM file into a Gramps database with user context.
+
+    This function has the right signature to be used as a Gramps import plugin.
+
+    Args:
+        database: The Gramps database to import the GEDCOM file into.
+        filename: The path to the GEDCOM file to import.
+        user: The user context for the import operation.
+    """
+    import_gedcom(filename, database)
