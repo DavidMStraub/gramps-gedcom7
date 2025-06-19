9c880682f99ec7bb363ca1753b4bc9ae8a907cfa
David Straub <straub@protonmail.com>
2025-06-19 14:12:39 +0200

    Rename function argument

--- gramps_gedcom7/importer.py
+++ gramps_gedcom7/importer.py
@@ -11,32 +11,32 @@ from typing import TextIO, BinaryIO
 from . import process
 
 
-def import_gedcom(file_input: str | Path | TextIO | BinaryIO, db: DbWriteBase) -> None:
+def import_gedcom(input_file: str | Path | TextIO | BinaryIO, db: DbWriteBase) -> None:
     """Import a GEDCOM file into a Gramps database.
 
     Args:
 
-        file_input: The GEDCOM file to import. This can be a string, Path object, or file-like object.
+        input_file: The GEDCOM file to import. This can be a string, Path object, or file-like object.
         db: The Gramps database to import the GEDCOM file into.
     """
-    # Check if file_input is a string or Path object
-    if isinstance(file_input, (str, Path)):
-        with open(file_input, "r", encoding="utf-8") as f:
+    # Check if input_file is a string or Path object
+    if isinstance(input_file, (str, Path)):
+        with open(input_file, "r", encoding="utf-8") as f:
             gedcom_data: str = f.read()
-    elif isinstance(file_input, TextIO):
-        gedcom_data = file_input.read()
-    elif isinstance(file_input, BinaryIO):
-        gedcom_data = file_input.read().decode("utf-8")
+    elif isinstance(input_file, TextIO):
+        gedcom_data = input_file.read()
+    elif isinstance(input_file, BinaryIO):
+        gedcom_data = input_file.read().decode("utf-8")
     else:
         raise TypeError(
-            "file_input must be a string, Path object, or file-like object."
+            "input_file must be a string, Path object, or file-like object."
         )
 
     gedcom_structures = gedcom7.loads(gedcom_data)
     process.process_gedcom_structures(gedcom_structures, db)
 
 
-def import_gedcom_gramps(database: DbWriteBase, filename: str, user: UserBase):
+def import_gedcom_gramps(database: DbWriteBase, filename: str, user: UserBase) -> None:
     """Import a GEDCOM file into a Gramps database with user context.
 
     This function has the right signature to be used as a Gramps import plugin.
